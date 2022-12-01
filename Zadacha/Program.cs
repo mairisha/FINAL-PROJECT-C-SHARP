4e7b73d7b6f7a452b0c689b703f119ec92bdfb7b
Ирина <mairisha@yandex.ru>
2022-12-01 15:00:38 +0300

    Code correction made

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,60 +1,64 @@
 ﻿// Из имеющегося массива строк сформировать массив строк длина которых меньше или равна 3.
 
-void FillArray(string []array)
+void PrintArray(string[] array)
 {
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write(array[i]+" ");
-}
-Console.WriteLine("\n");
-}
-
-string [] ArrayCount(string[]array)
-{
-int count=0;
-string [] result = new string [array.Length];
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length<=3)
+    for (int i = 0; i < array.Length; i++)
     {
-        count=count+1;
-        result[i-1]=array[i];   
+        Console.Write(array[i] + " ");
     }
+    Console.WriteLine("\n");
 }
-return result;
-}
-int ArrayLengthCount(string[]array)
+
+string[] CheckArray(string[] array)
 {
-int count=0;
-string [] result = new string [array.Length];
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length<=3)
+    int count = 0;
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
     {
-        count=count+1;
-        result[i-1]=array[i];   
+        if (array[i].Length <= 3)
+        {
+
+            result[count] = array[i];
+            count = count + 1;
+        }
     }
-}
-return count;
+    return result;
 }
 
-
-void PrintArray(string[]array,int count)
+int CheckCount(string[] array)
 {
-for (int i = 0; i < count; i++)
-{
-    Console.Write(array[i]+" ");
-}
-Console.WriteLine("\n");
+    int count = 0;
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
 }
 
-string [] str= {"Массив", "из" , "трех", "строк"};
+string[] FillNewArray(string[] array, int count)
+{
+    string[] NewArray = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        NewArray[i] = array[i];
+    }
+    return NewArray;
+}
+
+
+
+string[] str = { "Массив", "из", "трех", "строк", "'элементы", "0", "124" };
 Console.WriteLine("Исходный массив");
-FillArray(str);
-string[] newarray=ArrayCount(str);
-int value=ArrayLengthCount(str);
+PrintArray(str);
+string [] ArrayMiddle = CheckArray(str);
+int value = CheckCount(str);
+string [] NewArray = FillNewArray(ArrayMiddle, value);
 Console.WriteLine("Новый массив");
-PrintArray(newarray,value);
+PrintArray(NewArray);
 
 
 
